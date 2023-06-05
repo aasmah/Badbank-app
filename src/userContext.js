d62dcb6a8b1da64dae0bdf1469453c20c2ccea37
aasmah <35153854+aasmah@users.noreply.github.com>
2023-06-05 14:23:58 -0500

    Create userContext.js

---
+++ src/userContext.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+//Sample Data
+// import users from "../data/user";
+const UserContext = createContext({});
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState("");
+
+  return (
+    <>
+      <UserContext.Provider
+        value={{ user, setUser, loggedInUser, setLoggedInUser }}
+      >
+        {children}
+      </UserContext.Provider>
+    </>
+  );
+};
+
+export default UserContext;
